198b01cff7aaf101535a9d310ace1673d4bcce93
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2022-12-27 16:35:40 +0100

    added a pretty error page

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,5 +1,27 @@
-<?php
+<?php declare(strict_types = 1);
 
-declare(strict_types = 1);
+namespace Nelwhix\ContactForm;
 
-echo "Hello World!";
+use Dotenv\Dotenv;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+// Load all the environment variables
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+
+$whoops = new \Whoops\Run;
+if ($_ENV['APP_ENV'] !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function ($e) {
+        echo "Friendly Error Page";
+    });
+}
+
+$whoops->register();
+
+throw new \Exception;
