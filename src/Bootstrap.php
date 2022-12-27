99fd9643e16c7150b3b6df387d87ff9b48e852cf
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2022-12-27 17:11:56 +0100

    adding routes

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -12,7 +12,7 @@ error_reporting(E_ALL);
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-
+// Set up error page handling
 $whoops = new \Whoops\Run;
 if ($_ENV['APP_ENV'] !== 'production') {
     $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
@@ -24,4 +24,24 @@ if ($_ENV['APP_ENV'] !== 'production') {
 
 $whoops->register();
 
-throw new \Exception;
+// set up request/response object;
+$request = new  \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+$response = new \Http\HttpResponse;
+
+
+$routeDefinitionCallback = function (\FastRoute\RouteCollector $r) {
+    $routes = include('Routes.php');
+
+    foreach ($routes as $route) {
+        $r->addRoute($route[0], $route[1], $route[2]);
+    }
+};
+
+$dispatcher = \FastRoute\simpleDispatcher($routeDefinitionCallback);
+
+
+foreach ($response->getHeaders() as $header) {
+    header($header, false);
+}
+
+echo $response->getContent();
---
+++ src/Routes.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1);
+
+return [
+    [
+        'GET',
+        '/',
+        function () {
+            echo "Hello World";
+        }
+    ]
+];
