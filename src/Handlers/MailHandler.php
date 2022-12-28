cc1aa5a7b128c4e0107c910ddfccd7496efaa592
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2022-12-28 12:00:04 +0100

    setting up phpmailer

--- src/Handlers/MailHandler.php
+++ src/Handlers/MailHandler.php
@@ -4,6 +4,9 @@ namespace Nelwhix\ContactForm\Handlers;
 
 use Http\Request;
 use Http\Response;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 class MailHandler
 {
@@ -17,10 +20,27 @@ class MailHandler
         $this->response->setContent($index);
     }
 
-    public function send($request) {
-        var_dump($request);
-        die();
-        $response = implode(" ", );
-        $this->response->setContent($response);
+    public function send() {
+        $name = $this->request->getParameter("name");
+        $email = $this->request->getParameter("email");
+
+        $mail = new PHPMailer(true);
+        $mail->isSMTP();
+        $mail->Host = 'localhost';
+        $mail->Port = 25;
+
+        $mail->setFrom($email, $name);
+        $mail->addAddress("nelsonisioma1@gmail.com");
+
+        if ($mail->addReplyTo($email, $name)) {
+            $mail->Subject = "Testing PHPMailer";
+            $mail->isHTML(false);
+
+            $mail->Body = "You are gonna be great young padawan";
+
+            $mail->send();
+        }
+
+        $this->response->setContent("Email sent successfully");
     }
 }
